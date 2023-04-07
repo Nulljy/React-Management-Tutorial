23d4c0de04442fc1b5c06cb0d8c6ece1d66d9201
jin <jjy951122@naver.com>
2023-04-07 20:06:02 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+
+const customers = 
+  [
+    {id: 1,
+      image: 'https://placeimg.com/64/64/1',
+      name: '하니',
+      birthday: '951122',
+      gender: '여자',
+      job: '대학생',},
+    {id: 2,
+      image: 'https://placeimg.com/64/64/2',
+      name: '강해린',
+      birthday: '951122',
+      gender: '여자',
+      job: '대학생',},
+    {id: 3,
+      image: 'https://placeimg.com/64/64/3',
+      name: '민지',
+      birthday: '951122',
+      gender: '여자',
+      job: '대학생',}
+  ];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>management system</h2>
+    <div>
+      {customers.map(v => {
+        return <Customer
+        key = {v.id}
+        id = {v.id}
+        image = {v.image}
+        name = {v.name}
+        birthday = {v.birthday}
+        gender = {v.gender}
+        job = {v.job}
+      />
+    })}
     </div>
   );
 }
